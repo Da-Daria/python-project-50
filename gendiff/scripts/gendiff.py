4d223cd34cf39b664f83550ed6c4b993eab78784
Дарья Степанова <step.dasha.2000@gmail.com>
2025-03-11 19:31:20 +0300

    Изменения в файлах

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -2,11 +2,13 @@ import argparse
 
 def main():
     parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
-    parser.add_argument('first_file', help='First configuration file')
-    parser.add_argument('second_file', help='Second configuration file')
+    parser.add_argument('first_file', help='path to the first file')
+    parser.add_argument('second_file', help='path to the second file')
+    parser.add_argument('-f', '--format', default='plain', help='set format of output')
+
     args = parser.parse_args()
     
-    print(f'Comparing {args.first_file} and {args.second_file}')
+    print(f'Comparing {args.first_file} and {args.second_file} with format {args.format}')
 
 if __name__ == '__main__':
     main()
